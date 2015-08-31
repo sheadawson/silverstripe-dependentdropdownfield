be00fcaf36cb200b19a609be82c7cd289a04658b
3Dgoo <michael@3dgoo.com>
2015-08-31 15:03:19 +0930

    Minor JavaScript tweaks to pass JSHint
    Adding three semicolons to make the code pass JSHint validation. 
    
    Also a few white space additions as recommended by tidy up script.
    
    These edits are really minor. If they get rejected that's fine. Thanks for the great module.

--- javascript/dependentdropdownfield.js
+++ javascript/dependentdropdownfield.js
@@ -1,41 +1,44 @@
-jQuery.entwine("dependentdropdown", function($) {
+jQuery.entwine("dependentdropdown", function ($) {
 	
 	$(":input.dependent-dropdown").entwine({
-		onadd: function() {
+		onadd: function () {
 			var drop = this;
 			var depends = ($(":input[name=" + drop.data('depends').replace(/[#;&,.+*~':"!^$[\]()=>|\/]/g, "\\$&") + "]"));
 
 			this.parents('.field:first').addClass('dropdown');
 
 			depends.change(function () {
-				if(!this.value) {
+				if (!this.value) {
 					drop.disable(drop.data('unselected'));
 				} else {
-					drop.disable("Loading...")
+					drop.disable("Loading...");
 
-					$.get(drop.data('link'), { val: this.value }, function(data) {
+					$.get(drop.data('link'), {
+						val: this.value
+					},
+					function (data) {
 						drop.enable();
 
-						if(drop.data('empty') || drop.data('empty') === "") {
-							drop.append($("<option />").val("").text(drop.data('empty')))
+						if (drop.data('empty') || drop.data('empty') === "") {
+							drop.append($("<option />").val("").text(drop.data('empty')));
 						}
 
-						$.each(data, function() {
-							drop.append($("<option />").val(this.k).text(this.v))
+						$.each(data, function () {
+							drop.append($("<option />").val(this.k).text(this.v));
 						});
 						drop.trigger("liszt:updated");
 					});
 				}
 			});
 
-			if(!depends.val()) {
+			if (!depends.val()) {
 				drop.disable(drop.data('unselected'));
 			}
 		},
-		disable: function(text){
+		disable: function (text) {
 			this.empty().append($("<option />").val("").text(text)).attr("disabled", "disabled").trigger("liszt:updated");
 		},
-		enable: function(){
+		enable: function () {
 			this.empty().removeAttr("disabled").next().removeClass('chzn-disabled');
 		}
 	});
