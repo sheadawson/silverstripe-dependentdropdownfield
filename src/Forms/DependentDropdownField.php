b23ae3afff7a56421456773be45f2663200cf41e
Shawn Gray <shawn.gray@silverstripe.com>
2023-06-16 12:04:02 +1200

    Remove 'dist' from jquery-entwine requirement to allow front-end usage

--- src/Forms/DependentDropdownField.php
+++ src/Forms/DependentDropdownField.php
@@ -171,7 +171,7 @@ class DependentDropdownField extends DropdownField
     public function Field($properties = [])
     {
         if (!is_subclass_of(Controller::curr(), LeftAndMain::class)) {
-            Requirements::javascript('silverstripe/admin:thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
+            Requirements::javascript('silverstripe/admin:thirdparty/jquery-entwine/jquery.entwine.js');
         }
 
         Requirements::javascript(
