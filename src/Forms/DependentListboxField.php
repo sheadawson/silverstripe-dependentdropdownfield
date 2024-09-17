046716f455217acb2c853803d9ca65433ef4d1b5
Danni Dickson <danni.dickson@silverstripe.com>
2024-09-17 20:50:49 +1200

    Add listbox dependant field type

--- src/Forms/DependentListboxField.php
+++ src/Forms/DependentListboxField.php
@@ -4,21 +4,17 @@ namespace Sheadawson\DependentDropdown\Forms;
 
 use SilverStripe\Admin\LeftAndMain;
 use SilverStripe\Control\Controller;
-use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\ListboxField;
+use SilverStripe\ORM\Map;
 use SilverStripe\View\Requirements;
 use Sheadawson\DependentDropdown\Traits\DependentFieldTrait;
 
 /**
- * Class DependentDropdownField
- *
- * A dropdown that depends on another dropdown for populating values, and calls
- * a callback when that dropdown is updated.
  *
  * @package SilverStripe\Forms
  */
-class DependentDropdownField extends DropdownField
+class DependentListboxField extends ListboxField
 {
-
     use DependentFieldTrait;
 
     /**
@@ -26,19 +22,47 @@ class DependentDropdownField extends DropdownField
      * @param string $name
      * @param string $title
      * @param \Closure $source
-     * @param string $value
-     * @param $form
-     * @param string $emptyString
+     * @param string|array|null $value
+     * @param int $size
      */
-    public function __construct($name, $title = null, \Closure $source = null, $value = '', $form = null, $emptyString = null)
+    public function __construct($name, $title = null, \Closure $source = null, $value = null, $size = null)
     {
-        parent::__construct($name, $title, [], $value, $form, $emptyString);
+        parent::__construct($name, $title, [], $value, $size);
 
         // we are unable to store Closure as a normal source
         $this->sourceCallback = $source;
         $this
             ->addExtraClass('dependent-dropdown')
+            ->addExtraClass('listbox')
             ->addExtraClass('dropdown');
+    }
+
+    /**
+     * @return array|\ArrayAccess|mixed
+     */
+    public function getSource()
+    {
+        $val = $this->depends->Value();
+
+        if (
+            !$val
+            && method_exists($this->depends, 'getHasEmptyDefault')
+            && !$this->depends->getHasEmptyDefault()
+        ) {
+            $dependsSource = array_keys($this->depends->getSource());
+            $val = isset($dependsSource[0]) ? $dependsSource[0] : null;
+        }
+
+        if (!$val) {
+            $source = [];
+        } else {
+            $source = call_user_func($this->sourceCallback, $val);
+            if ($source instanceof Map) {
+                $source = $source->toArray();
+            }
+        }
+
+        return $source;
     }
 
     /**
@@ -48,7 +72,7 @@ class DependentDropdownField extends DropdownField
     public function Field($properties = [])
     {
         if (!is_subclass_of(Controller::curr(), LeftAndMain::class)) {
-            Requirements::javascript('silverstripe/admin:thirdparty/jquery-entwine/jquery.entwine.js');
+            Requirements::javascript('silverstripe/admin:thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
         }
 
         Requirements::javascript(
@@ -57,7 +81,6 @@ class DependentDropdownField extends DropdownField
 
         $this->setAttribute('data-link', $this->Link('load'));
         $this->setAttribute('data-depends', $this->getDepends()->getName());
-        $this->setAttribute('data-empty', $this->getEmptyString());
         $this->setAttribute('data-unselected', $this->getUnselectedString());
 
         return parent::Field($properties);
